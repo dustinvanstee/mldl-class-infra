27f888b1e0cf92c2ccb7bf8d060a9540d814f234
dustinvanstee <dustinvanstee@gmail.com>
2017-10-14 10:27:30 -0400

    fixed up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd /tmp/
-git clone https://github.com/dustinvanstee/mldl-101.git
-/sbin/init
+GPU_MODE=1
 
+cd /home/nimbix
+git clone https://github.com/dustinvanstee/mldl-101.git
+chown -R nimbix:nimbix mldl-101
+
+cd /home/nimbix/mldl-101
+source startClass2.sh -g $GPU_MODE

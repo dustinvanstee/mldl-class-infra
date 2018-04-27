c02a65c46e8eaa5d8dc5cd2b772c2c3e937e0316
Dustin <dustinvanstee@gmail.com>
2018-04-27 07:23:00 -0400

    Update startjupyter.sh

--- startjupyter.sh
+++ startjupyter.sh
@@ -2,4 +2,4 @@
 source /root/yololab_env/bin/activate
 cd /dl-labs/mldl-101
 sudo git pull origin master
-sudo jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --config /dl-labs/.jupyter/jupyter_notebook_config.json  &
+sudo jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --config /dl-labs/.jupyter/jupyter_notebook_config.json

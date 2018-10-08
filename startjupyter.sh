18c52b1c4763c3632cceeb389a9fe00f87af78a5
vanstee <vanstee@apache.org>
2018-10-08 10:44:18 -0400

    upd

--- startjupyter.sh
+++ startjupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /root/yololab_env/bin/activate
+# source /root/yololab_env/bin/activate
 cd /dl-labs/mldl-101
 jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --config /dl-labs/.jupyter/jupyter_notebook_config.json

c4c2feae92982b6bcf551446ab01596a9db5a7f7
vanstee <vanstee@apache.org>
2018-10-10 08:20:37 -0400

    upd

--- startjupyter.sh
+++ startjupyter.sh
@@ -10,14 +10,22 @@ source /opt/DL/tensorflow/bin/tensorflow-activate
 
 CUDA_VERSION_JS=cuda-9.2
 FILE=/usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/libcuda.so.1
-if [ -f $FILE ]; then
-   echo "File $FILE exists. Doing nothing, must be on a GPU box"
-else
-   echo "File $FILE does not exist. Must be on a CPU box .."
+
+# Hack... if nvidia-smi exists, then assume gpu machine, else cpu link required
+nvidia_smi_path=`which nvidia-smi`
+has_gpu=${#nvidia_smi_path}
+if [ "$has_gpu" -gt "0" ]; then
+  echo "GPU"
+else 
+  echo "CPU"
+     echo "File $FILE does not exist. Must be on a CPU box .."
    echo linking....
    ln -fs  /usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/libcuda.so $FILE  
    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/
+
 fi
+
+
 
 cd /dl-labs/mldl-101
 jupyter notebook --ip=0.0.0.0 --allow-root --port=5050 --no-browser --config /dl-labs/.jupyter/jupyter_notebook_config.json

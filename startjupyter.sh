31c3bdec5ca2acfe24c0daeeac050b5d66e3fde2
vanstee <vanstee@apache.org>
2018-10-10 10:32:42 -0400

    techu hfl

--- startjupyter.sh
+++ startjupyter.sh
@@ -12,17 +12,14 @@ CUDA_VERSION_JS=cuda-9.2
 FILE=/usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/libcuda.so.1
 
 # Hack... if nvidia-smi exists, then assume gpu machine, else cpu link required
-nvidia_smi_path=`which nvidia-smi`
-has_gpu=${#nvidia_smi_path}
-if [ "$has_gpu" -gt "0" ]; then
-  echo "GPU"
+if [ -f /usr/bin/nvidia-smi ]; then
+    echo "GPU"
 else 
-  echo "CPU"
-     echo "File $FILE does not exist. Must be on a CPU box .."
-   echo linking....
-   ln -fs  /usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/libcuda.so $FILE  
-   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/
-
+    echo "CPU"
+    echo "File $FILE does not exist. Must be on a CPU box .."
+    echo linking....
+    ln -fs  /usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/libcuda.so $FILE  
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/$CUDA_VERSION_JS/targets/ppc64le-linux/lib/stubs/
 fi
 
 
